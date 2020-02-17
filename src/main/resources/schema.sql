cad0c3a39db43d14aa1046cab14d7c48046dda4b
Joseph Shaferman <joseph.shaferman@gmail.com>
2020-02-17 11:37:16 +0200

    add sql schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE keyword (
+    id serial primary key,
+    label varchar(36) unique not null
+);
+
+CREATE TABLE recipe (
+    id serial primary key,
+    description text not null,
+    inputs jsonb not null
+);
+
+CREATE TABLE keyword_recipe (
+    id serial primary key,
+    keyword_id integer not null references keyword(id),
+    recipe_id integer not null references recipe(id)
+);
+
+CREATE TABLE command (
+    id serial primary key,
+    inputs jsonb not null,
+    bin varchar(128) not null,
+    npm varchar(128),
+    github varchar(128)
+);
